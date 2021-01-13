604816f17ef331f56298026a5cbc9333ccf8299d
ilker <ilkerpeker79@yahoo.com>
2021-01-13 12:28:39 +0000

    Bonus1

--- main.js
+++ main.js
@@ -2,16 +2,19 @@
 const hand = document.querySelector(".hand");
 
 // Move the hand of a 🕗 every second using setInterval, 360 degrees in 60 seconds
-hand.style.transform = `rotate(6deg)`; // example of how to rotate the hand
+//hand.style.transform = `rotate(6deg)`; // example of how to rotate the hand
 
 //write a function to change the position of the hand.
 //create a variable and loop to increment the degree
-function changeHand() {
-  let i = 0;
-  for (; i <= 360; i += 6) {
-    console.log(`rotate(${i}deg)`);
-    // need to time each move to make it visible - wait for a second
-  }
-}
 
-setInterval(changeHand, 1000);
+// let i = 0;
+// function changeHand() {
+//   hand.style.transform = `rotate(${i}deg)`;
+//   i = i + 6;
+//   console.log(i);
+// }
+// setInterval(changeHand, 1000);
+
+//Bonus task
+let date = new Date();
+console.log(date.getSeconds());
