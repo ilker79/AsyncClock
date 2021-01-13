84e8a9c673b721a415ddc25200f8d61bcc3368db
ilker <ilkerpeker79@yahoo.com>
2021-01-13 11:53:19 +0000

    function created to  change the hand

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-
-
-
-//Select the hand  
+//Select the hand
 const hand = document.querySelector(".hand");
 
 // Move the hand of a 🕗 every second using setInterval, 360 degrees in 60 seconds
-hand.style.transform = `rotate(30deg)`; // example of how to rotate the hand
-
-
+hand.style.transform = `rotate(6deg)`; // example of how to rotate the hand
+//write a fucntion to change the position of the hand.
+//create a variable and loop to increment the degree
+function changeHand() {
+  for (let i = 0; i < 360; i += 6) {
+    hand.style.transform = `rotate(${i}deg)`;
+  }
+}
+changeHand();
