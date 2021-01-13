d244ac5a576a19d74e8d7cfd33682318a2f32493
Tibz <thibaut_marechal@yahoo.com>
2021-01-13 11:44:49 +0000

    problem break down

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+
+
+
+//Select the hand  
 const hand = document.querySelector(".hand");
 
-hand.style.transform = `rotate(90deg)`; // example of how to rotate the hand
+// Move the hand of a 🕗 every second using setInterval, 360 degrees in 60 seconds
+hand.style.transform = `rotate(30deg)`; // example of how to rotate the hand
+
+
